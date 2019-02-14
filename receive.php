986b8ad19a037f91dc16cf29ee2786b028d69523
DinoTsai <fumlinlin0210@hotmail.com>
2019-02-14 14:05:58 +0800

    Update receive.php

--- receive.php
+++ receive.php
@@ -49,7 +49,7 @@
 			
   	fwrite($myfile, "\xEF\xBB\xBF".json_encode($response)); //在字串前面加上\xEF\xBB\xBF轉成utf8格式
   	$header[] = "Content-Type: application/json";
-  	$header[] = "Authorization: Bearer gd1gyH+Pc5TROu9ku5u/5tDvFnffsU8nXU69zXuhTgE0dIS5nVGmx9Js8PwijeUqgFuwWXzyJ14/N5FUmp/UXsmSJbUsxMGA6AW1gozlf6cbEgSGLiC02BEaRa5wUSqE7df8FOANP1WjPW8Mh/TgtwdB04t89/1O/w1cDnyilFU=";
+  	$header[] = "Authorization: Bearer aYYNI1Vq78NwOZcv+S+fm2tvnRTDPgcNbE9QTmlQ4Q4RGXjK6xca/wGFMMXyBIE6cc2zI07wIX5LSUTCht2K4hmdTT2fOZ2XoXxP4LQJtP326V2yVn7nLaG+PcyIktlRw2iP582whaZEP5eJW31VfgdB04t89/1O/w1cDnyilFU=";
   	$ch = curl_init("https://api.line.me/v2/bot/message/reply");
   	curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
   	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));                                                                  
